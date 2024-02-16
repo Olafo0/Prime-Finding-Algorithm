fd3cdaa33cb86cde1ff5e1e968921e9cedb36bff
Olafo0 <opaladzs@gmail.com>
2024-02-16 22:41:13 +0000

    Added a timer.

--- PrimeNumberFinder/Program.cs
+++ PrimeNumberFinder/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Numerics;
 using System.Reflection.Metadata;
 using System.Threading.Tasks.Dataflow;
@@ -7,9 +9,50 @@ namespace PrimeNumberFinder
 {
     internal class Program
     {
+
+
+        private static void Cal(int limit)
+        {
+            List<int[]> numberPool = new List<int[]>();
+            int[] firstarray = new int[] { 2, 4 };
+            numberPool.Add(firstarray);
+
+            List<int> primeNumbers = new List<int>
+                    {
+                        firstarray[0]
+                    };
+
+            long startTimer = Stopwatch.GetTimestamp();
+            for (int i = 3; i < limit; i++)
+            {
+                numberPool = numberPool.OrderBy(array => array[1]).ToList();
+                if (i < numberPool[0][1])
+                {
+                    primeNumbers.Add(i);
+                    int newSquaredNumber = (int)Math.Pow(i, 2);
+                    int[] newLocalPrimeArray = new int[] { i, newSquaredNumber };
+                    numberPool.Add(newLocalPrimeArray);
+                }
+                else if (i >= numberPool[0][1])
+                {
+                    var eee = numberPool.Where(x => x[1] == numberPool[0][1]).ToList();
+                    foreach (var items in eee)
+                    {
+                        items[1] += items[0];
+                    }
+                }
+            }
+            TimeSpan time = Stopwatch.GetElapsedTime(startTimer);
+            string primeNumberStringArray = string.Join(" ", primeNumbers);
+            Console.WriteLine(primeNumberStringArray);
+            Console.WriteLine($"Time took: {time}");
+            Console.Write("PRESS ANY KEY TO GO BACK");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
         static void Main(string[] args)
         {
-            
             try
             {
                 while (true)
@@ -29,7 +72,7 @@ namespace PrimeNumberFinder
                         try
                         {
                             limit = int.Parse(stringLimit);
-                            flag = false; 
+                            flag = false;
                         }
                         catch
                         {
@@ -38,42 +81,9 @@ namespace PrimeNumberFinder
                             Console.WriteLine("The range of prime numbers you want to see 2 - X");
                             Console.Write("ENTER :");
                         }
+
+                        Cal(limit);
                     }
-           
-                    List<int[]> numberPool = new List<int[]>();
-                    int[] firstarray = new int[] { 2, 4 };
-                    numberPool.Add(firstarray);
-
-                    List<int> primeNumbers = new List<int>
-            {
-                firstarray[0]
-            };
-
-                    for (int i = 3; i < limit; i++)
-                    {
-                        numberPool = numberPool.OrderBy(array => array[1]).ToList();
-                        if (i < numberPool[0][1])
-                        {
-                            primeNumbers.Add(i);
-                            int newSquaredNumber = (int)Math.Pow(i, 2);
-                            int[] newLocalPrimeArray = new int[] { i, newSquaredNumber };
-                            numberPool.Add(newLocalPrimeArray);
-                        }
-                        else if (i >= numberPool[0][1])
-                        {
-                            var eee = numberPool.Where(x => x[1] == numberPool[0][1]).ToList();
-                            foreach (var items in eee)
-                            {
-                                items[1] += items[0];
-                            }
-                        }
-                    }
-
-                    string primeNumberStringArray = string.Join(" ", primeNumbers);
-                    Console.WriteLine(primeNumberStringArray);
-                    Console.Write("PRESS ANY KEY TO GO BACK");
-                    Console.ReadKey();
-                    Console.Clear();
                 }
             }
             catch (Exception ex)
